8bb659aaf90884ba74df5b7579f911012405e5e9
Leonardo Doria <leomvdoria@gmail.com>
2026-02-12 00:22:03 -0300

    Commit inicial

---
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,34 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+//RestController indica ao Spring que a classe é um controller
+//RequestMapping define a url padrão da classe (nesse caso, /medicos)
+@RestController
+@RequestMapping("/medicos")
+public class MedicoController {
+
+    //Injeção de dependencias: Autowired instancia o repositório
+    @Autowired
+    private MedicoRepository repository;
+
+    //POST
+    @PostMapping
+    //Insert no banco de dados
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
+        repository.save(new Medico(dados));
+
+    }
+
+}
